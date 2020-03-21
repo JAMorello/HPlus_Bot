9843f2893c0333a1bb40fe3b7ee8e1ec2b22b5af
JAMorello <juan.ag.morello@gmail.com>
2020-03-20 23:27:28 -0300

    Add three new functions and clean some code
    Add the function to_json(), retweet(), ratio_of_likes(). Cleaned a bit
    the code. The code implements the scheduler but is commented out to leave
    the first real call to the function when more features are added to the
    project. The check_recent_tweets() and search_for_users() shouldn´t
    be used anymore - or at least in the foreseable future.

--- Bot_REST.py
+++ Bot_REST.py
@@ -1,11 +1,139 @@
 import Setup
 import tweepy
 import json
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 
-def check_tweets(api, username):
+def to_json(twitter_object):
+    # The object that the API give us about the user or the tweets is messy.
+    # We need a json to properly access the data.
+    json_str = json.dumps(twitter_object._json)  # convert to string
+    return json.loads(json_str)  # deserialise string into python object
+
+
+def retweet(api):
     """
-    With this function we can make sure that the user passed as parameter has made tweets of retweets
+    The function retweets all the tweets (no retweets) of the users that the bot follows.
+    Specifically retweets those who comply to the criteria of keywords and/or hashtags, have a certain minimum number
+    of likes, and were posted after a certain tweet (that is stored in Last_Tweet_ID.txt). At the end, the ID of the
+    last tweet retweeted is stored in the .txt to serve as the starting point of the next call to the function.
+    """
+
+    file = open("Last_Tweet_ID.txt")
+    last_tweet_id = int(file.read())  # ID of last status retweeted
+    file.close()
+
+    users_followed = api.friends_ids(screen_name="HPlusBot")  # List of IDs of users that the bot follows
+
+    for user in users_followed:
+
+        tweets_list = tweepy.Cursor(api.user_timeline, id=user, tweet_mode='extended',
+                                    since_id=last_tweet_id).items()
+
+        for tweet in tweets_list:
+
+            parsed_tweet = to_json(tweet)
+            tweet_text = parsed_tweet["full_text"].lower().split()
+
+            if not tweet_text[0] == "rt" and parsed_tweet["favorite_count"] >= Setup.LIKES_TO_RETWEET:
+                # Checks if the status is not a retweet and if it has at least x likes required
+
+                if any(elem in Setup.HASHTAGS for elem in tweet_text) or \
+                        any(elem in Setup.KEYWORDS for elem in tweet_text):
+                    # Checks if the tweet matches the criteria of keywords or hashtags (at least one element of any
+                    # of those)
+
+                    api.retweet(id=parsed_tweet["id"])
+
+                    if parsed_tweet["id"] > last_tweet_id:
+                        last_tweet_id = parsed_tweet["id"]
+
+    print("Retweeting done!")
+
+    with open('Last_Tweet_ID.txt', 'w') as file:
+        file.write(str(last_tweet_id))
+
+
+def ratio_of_likes(api):
+    """
+    The purpose of this function is to determinate a certain number of likes that is going to be used as a criteria
+    that the tweets need to match to be retweeted. To do so, the function iterates trough the tweets of the users
+    that the bot follows and gets the likes of those tweets. The function selects only the tweets that were
+    posted after a certain tweet (that is stored in Last_Tweet_ID.txt) and specifically those who comply to the criteria
+    of keywords and/or hashtags.
+    Two dictionaries store the data: one for the number of tweets that have between 0-49, between 50-499, and more
+    than 500 likes; and another for the total of likes between those ranges. The function then prints the ranges,
+    number of tweets in each range, the total of likes in each range, and the ratio
+    (total of likes / number of tweets in that range).
+    """
+
+    total = {
+        "500<<": 0,
+        "50-499": 0,
+        "0-49": 0
+    }
+    quantity = {
+        "500<<": 0,
+        "50-499": 0,
+        "0-49": 0
+    }
+    total_tweets = 0
+
+    file = open("Last_Tweet_ID.txt")
+    last_tweet_id = file.read()  # ID of last status retweeted
+    file.close()
+
+    users_followed = api.friends_ids(screen_name="HPlusBot")   # List of IDs of users that the bot follows
+
+    for user in users_followed:
+
+        tweets_list = tweepy.Cursor(api.user_timeline, id=user, tweet_mode='extended',
+                                    since_id=last_tweet_id).items()
+
+        for tweet in tweets_list:
+
+            parsed = to_json(tweet)
+            total_tweets += 1
+
+            tweet_text = parsed["full_text"].lower().split()
+
+            if not tweet_text[0] == "rt":  # Checks if the status is not a retweet
+
+                if any(elem in Setup.HASHTAGS for elem in tweet_text) or \
+                        any(elem in Setup.KEYWORDS for elem in tweet_text):
+
+                    if parsed["favorite_count"] >= 500:
+                        total["500<<"] += parsed["favorite_count"]
+                        quantity["500<<"] += 1
+                    elif parsed["favorite_count"] >= 50:
+                        total["50-499"] += parsed["favorite_count"]
+                        quantity["50-499"] += 1
+                    else:
+                        total["0-49"] += parsed["favorite_count"]
+                        quantity["0-49"] += 1
+                    print("Done ", total_tweets, parsed["user"]["screen_name"])
+
+    print(total_tweets)
+
+    if quantity["500<<"] > 0:
+        print("500>> = ", quantity["500<<"])
+        print("Total = ", total["500<<"])
+        print("Ratio = ", total["500<<"] / quantity["500<<"], "\n")
+
+    if quantity["50-499"] > 0:
+        print("50-499 = ", quantity["50-499"])
+        print("Total = ", total["50-499"])
+        print("Ratio = ", total["50-499"] / quantity["50-499"], "\n")
+
+    if quantity["0-49"] > 0:
+        print("0-49 = ", quantity["0-49"])
+        print("Total = ", total["0-49"])
+        print("Ratio = ", total["0-49"] / quantity["0-49"], "\n")
+
+
+def check_recent_tweets(api, username):
+    """
+    With this function we can make sure that the user passed as parameter has made tweets or retweets
     about the subjects of our interest (passed here as the Keywords and Hashtags in Setup.py). A minimum of x
     tweets (in this case, 2) must be reached for the user be eligible to be followed.
     """
@@ -16,13 +144,12 @@ def check_tweets(api, username):
     for tweet in tweets_list:
         # The info that the API gave us about the user is messy.
         # We need a json to properly access the data.
-        json_str = json.dumps(tweet._json)  # convert to string
-        parsed = json.loads(json_str)  # deserialise string into python object
+        parsed = to_json(tweet)
 
         tweet_text = parsed["text"].lower().split()  # A list that contains all the words of the tweet
 
         if any(elem in Setup.KEYWORDS for elem in tweet_text) or \
-           any(elem in Setup.HASHTAGS for elem in tweet_text):
+                any(elem in Setup.HASHTAGS for elem in tweet_text):
             counter += 1
 
     # At least 2 recent tweets must match our criteria of keywords and hashtags
@@ -44,17 +171,14 @@ def search_for_users(api):
     """
 
     user_list = tweepy.Cursor(api.search_users, q='transhumanism').items(200)
-    users_data = [] # Here we are going to store the users information
+    users_data = []  # Here we are going to store the users information
 
     for user in user_list:
-        # The object that the API give us about the user is messy.
-        # We need a json to properly access the data.
-        json_str = json.dumps(user._json)  # convert to string
-        parsed = json.loads(json_str)  # deserialise string into python object
+        parsed = to_json(user)
 
         try:
             if parsed["followers_count"] > 1000 and not parsed["following"]:
-                if check_tweets(api, parsed["screen_name"]):
+                if check_recent_tweets(api, parsed["screen_name"]):
                     user = {
                         "name": parsed["screen_name"],
                         "followers": parsed["followers_count"],
@@ -73,4 +197,8 @@ def search_for_users(api):
 
 if __name__ == "__main__":
     api = Setup.setup()
-    search_for_users(api)
+
+    # The retweet() function is going to be called every two hours
+    # scheduler = BlockingScheduler()
+    # scheduler.add_job(retweet, 'interval', args=[api], hours=2)
+    # scheduler.start()
