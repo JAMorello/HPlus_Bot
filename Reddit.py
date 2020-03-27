b0a708b751a06f326de0232fa01d78b82e8ac6eb
JAMorello <juan.ag.morello@gmail.com>
2020-03-27 18:55:53 -0300

    Add file to project The file contains a TODO. Must figure out how to access the Reddit API

---
+++ Reddit.py
@@ -0,0 +1,6 @@
+# CHECK REDDIT API ---> https://www.reddit.com/dev/api
+# SEE VIDEOS ON YOUTUBE
+# https://www.reddit.com/r/Transhuman/
+# https://www.reddit.com/r/transhumanism/
+# https://www.reddit.com/r/singularity/
+# https://www.reddit.com/r/Futurology/
