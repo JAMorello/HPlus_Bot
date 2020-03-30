b58b9ae4fb7a2df56cd87530041a80f04507b060
JAMorello <juan.ag.morello@gmail.com>
2020-03-30 14:38:44 -0300

    Update file Update imports, add global variables, update function

--- HPlus_Pedia.py
+++ HPlus_Pedia.py
@@ -1,17 +1,24 @@
 import requests
+# https://requests.readthedocs.io/en/master/
 from bs4 import BeautifulSoup
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+from Bot_REST import post_tweet
+
+HPLUSPEDIA_MAINPAGE = "https://hpluspedia.org/wiki/Main_Page"
+HPLUSPEDIA_URL = 'https://hpluspedia.org'
 
 
-def random_page():
+def random_page(api):
     """
     This functions goes to the Random Articles section of the Main Page of H+Pedia wiki.
     Scraps the link and title of the first article an that section. Returns link and title
     in a formatted sentence to be posted in Twitter.
     """
-    response = requests.get("https://hpluspedia.org/wiki/Main_Page")
+    response = requests.get(HPLUSPEDIA_MAINPAGE)
     soup_parsed_response = BeautifulSoup(response.text, 'html.parser')
 
     first_random_page = soup_parsed_response.select('.randomPages')[0].find('li')
-    url = 'https://hpluspedia.org' + first_random_page.a['href']
+    url = HPLUSPEDIA_URL + first_random_page.a['href']
     title = first_random_page.a['title']
-    return f"Learn more about {title} in HPlusPedia! Go to: {url}"
+    print("Tweeting random page from H+Pedia")
+    post_tweet(api, f"Learn more about {title} in HPlusPedia! Go to: {url}")
