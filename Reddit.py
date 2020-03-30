2897be07a35196f009922f03d7d827748e356ad3
JAMorello <juan.ag.morello@gmail.com>
2020-03-30 14:42:11 -0300

    Add file to project The files contain the scrapper script of Reddit submissions

--- Reddit.py
+++ Reddit.py
@@ -1,6 +1,22 @@
-# CHECK REDDIT API ---> https://www.reddit.com/dev/api
-# SEE VIDEOS ON YOUTUBE
-# https://www.reddit.com/r/Transhuman/
-# https://www.reddit.com/r/transhumanism/
-# https://www.reddit.com/r/singularity/
-# https://www.reddit.com/r/Futurology/
+import Setup
+import time
+from Bot_REST import post_tweet
+
+SUBREDDITS = ['Transhuman', 'Transhumanism', 'Singularity', 'Futurology']
+
+
+def start_stream(api):
+    reddit = Setup.setup_reddit()
+    subreddits = reddit.subreddit('+'.join(SUBREDDITS))
+
+    for submission in subreddits.stream.submissions():
+        url = submission.url
+        if 'www.reddit.com' not in url and 'i.redd.it' not in url:
+            if len(submission.title) > 100:
+                title = submission.title[:100] + '...'
+            else:
+                title = submission.title
+            print("Tweeting data from Reddit")
+            post_tweet(api, f"Check out \"{title}\" at: {url}")
+            time.sleep(60)  # sleeps for one minute to avoid twitter api and reddit api limit rate
+            # also to avoid flooding twitter timeline
